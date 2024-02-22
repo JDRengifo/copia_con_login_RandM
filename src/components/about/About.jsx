8e2f08f540f1448a89c333f474031610576208d0
JDRengifo <josedariorf@gmail.com>
2024-02-22 20:49:03 -0300

    MM3

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,7 @@ function About(){
             <img src="./foto_de_frente.png" alt="foto de frente" />
             <h1>SOY HENRY</h1>
             <br />
-            <h2>"PROYECTO RICK and MORTY"</h2>
+            <h2>"PROYECTO RICK AND MORTY"</h2>
             <br />
             <h2>Esta aplicación esta siendo creada por José Rengifo</h2>
             <h2>La historia de las tarjetas está ambientada en la serie televisiva 'Rick and Morty' que se inspira en la película 'Volver al Futuro'
